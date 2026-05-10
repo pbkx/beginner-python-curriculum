c0cbd8ea6567b65a3a24b558cfdc8558480ae061
steamproof <93405617+pbkx@users.noreply.github.com>
2026-05-09 18:01:15 -0700

    Finished lesson 1

---
+++ lesson1/classwork/printing.py
@@ -0,0 +1,6 @@
+print("Hello world!")
+print("My name is Max!")
+print('My favorite food is pizza!')
+
+# Error: mismatched quotation marks
+# print("I love dogs!')
---
+++ lesson1/classwork/variables.py
@@ -0,0 +1,10 @@
+x = 5  # integer: whole number
+print(x)
+
+Number = 3.97  # float: number with a decimal point
+print(Number)
+
+favorite_food = "pizza"  # string: text enclosed in quotes
+print("My favorite food is", favorite_food)
+
+print(4.21, "dogs", x)
